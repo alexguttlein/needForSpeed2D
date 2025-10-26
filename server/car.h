26e295f92cd8080a4e8b0eb732371872ded11021
ValentinProvostFIUBA <vprovost@fi.uba.ar>
2025-10-26 18:25:56 -0300

    chore: refactor car more simple, for now is an struct

--- server/car.h
+++ server/car.h
@@ -3,74 +3,7 @@
 
 #include <cmath>
 
-class Car {
-
-private:
-    float x, y;          
-    float speed;
-    float direction;
-    float acceleration;
-    float health;
-
-public:
-
-    /*
-    * Constructor de la clase Car
-    *
-    * */
-    explicit Car(float start_x, float start_y);
-    
-    /*
-    * Acelera el auto
-    *
-    *  */
-    void accelerate(float distance);
-
-    /*
-    * Gira el auto a la derecha
-    *
-    * */
-    void turnRight(float angle);
-    
-
-    /*
-    * Gira el auto a la izquierda
-    * */
-    void turnLeft(float angle);
-
-
-    /*
-    * Mueve el auto
-    *
-    * */
-    void move(float distance);
-
-
-    /*
-    * Retrocede el auto
-    *
-    * */
-    void reverse(float distance);
-
-
-    /*
-    * Obtiene la posición x del auto
-    *
-    * */
-    float getX() const;
-
-
-    /*
-    * Obtiene la posición y del auto
-    *
-    * */
-    float getY() const;
-
-
-    /*
-    * Obtiene la salud del auto
-    *
-    * */
-    float getHealth() const;
+struct Car {
+    float x, y;
 };
 #endif // CAR_H
