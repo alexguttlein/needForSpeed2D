21a201cc65d9d60f2d37f07e1bf78c4aae8eacd8
ValentinProvostFIUBA <vprovost@fi.uba.ar>
2025-10-27 14:38:45 -0300

    [feat]: adding gameloop class, it has 1 fuction no implement yet, that it has to be implemented in client handler

---
+++ server/server_gameloop.cpp
@@ -0,0 +1,49 @@
+#include "server_gameloop.h"
+
+GameLoop::GameLoop(Queue<std::string>& commandQueue, MonitorClients& clients)
+    : running(false), clients(clients), commandQueue(commandQueue) {}
+
+
+void GameLoop::run() {
+    running = true;
+
+    while (running) {
+   
+        clients.killDisconnectedClients();
+        processCommandQueue();
+        broadcastSnapshots();
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));  // ~20 FPS
+    }
+}
+
+
+void GameLoop::processCommandQueue() {
+    std::string command;
+    while (commandQueue.try_pop(command)) {
+        std::cout << "Comando recibido: " << command << std::endl;
+    }
+}
+
+
+void GameLoop::broadcastSnapshots() {
+    clients.forEachClient([this](ClientHandler& client){
+        Snapshot snapshot;
+        if (client.getSnapshots().popSnapshot(snapshot)) {
+            Message msg;
+            msg.code = 1;      // codigo de respuesta de snapshot? 
+            msg.car = snapshot.car;
+
+            clients.broadcastToAllClients(msg);
+        }
+    });
+}
+
+
+void GameLoop::stop() {
+    running = false;
+}
+
+
+GameLoop::~GameLoop() {
+    stop();
+}
---
+++ server/server_gameloop.h
@@ -0,0 +1,56 @@
+#ifndef SERVER_GAMELOOP_H
+#define SERVER_GAMELOOP_H
+
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#include "server_monitorClients.h"
+#include "server_snapshots.h"
+#include "../common/car.h"
+#include "../common/thread.h"
+
+class GameLoop : public Thread {
+
+private:
+    std::atomic<bool> running;
+    MonitorClients& clients;
+    Queue<std::string>& commandQueue;
+
+    void run() override;
+
+public:
+
+    /*
+    * Constructor de GameLoop
+    *
+    * */
+    explicit GameLoop(Queue<std::string>& commandQueue, MonitorClients& clients);
+
+    /*
+    * Envía snapshots a todos los clientes conectados
+    *
+    * */
+    void broadcastSnapshots();
+    
+    /*
+    * Procesa la cola de comandos recibidos
+    *
+    * */
+    void processCommandQueue();
+
+    /*
+    * Detiene el GameLoop
+    *
+    * */
+    void stop() override;
+    
+    /*
+    * Destructor de GameLoop
+    *
+    * */
+    ~GameLoop();
+};
+#endif // SERVER_GAMELOOP_H
